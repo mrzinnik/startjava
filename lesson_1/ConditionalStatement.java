b52917565977e0cd7968dec483d7f99cc51f4d2b
Kirill Zinabutdinov <zinabutdinov.ka@gmail.com>
2021-08-17 12:56:58 +0300

    Поправил оформление кода класса ConditionalStatement

--- lesson_1/ConditionalStatement.java
+++ lesson_1/ConditionalStatement.java
@@ -3,10 +3,10 @@ public class ConditionalStatement {
     public static void main(String[] args) {
         int age = 21;
         if(age > 20) {
-            System.out.println("Person is over 20 years old")
+            System.out.println("Person is over 20 years old");
         }
 
-        boolean isMale = true;
+        boolean isMale = false;
         if(isMale) {
             System.out.println("Person is male");
         }
@@ -14,15 +14,14 @@ public class ConditionalStatement {
             System.out.println("Person is female");
         }
         
-
-        double height = 1.9;
+        double height = 1.7;
         if(height < 1.80) {
             System.out.println("Person's height is less than 1.8 meters");
         } else {
             System.out.println("Person's height is equals or more than 1.8 meters");
         }
 
-        char nameFirstChar = 'K';
+        char nameFirstChar = 'I';
         if(nameFirstChar == 'M') {
             System.out.println("Person's name starts with letter M");
         } else if(nameFirstChar == 'I') {
