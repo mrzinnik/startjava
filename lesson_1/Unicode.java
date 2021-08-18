de6823846b8d89a11417c5435d9733161846be25
Kirill Zinabutdinov <zinabutdinov.ka@gmail.com>
2021-08-18 23:02:36 +0300

    Реализовал класс Unicode

---
+++ lesson_1/Unicode.java
@@ -0,0 +1,11 @@
+public class Unicode {
+    
+    public static void main(String[] args) {
+        char symbol;
+        for(int i = 33; i <= 126; i++) {
+            symbol = (char) i;
+            System.out.print(symbol + " ");
+        }
+        System.out.println();
+    }
+}
