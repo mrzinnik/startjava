17b62a4a25350f3ba979906f27e44c05562ec928
Kirill Zinabutdinov <zinabutdinov.ka@gmail.com>
2021-08-19 16:27:58 +0300

    Реализовал класс Person

---
+++ lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String gender = "male";
+    String name = "John";
+    int height = 182;
+    int weight = 75;
+    int age = 29;
+
+    void move() {
+        System.out.println("Moving");
+    }
+
+    void sit() {
+        System.out.println("Sitting");
+    }
+
+    void run() {
+        System.out.println("Running");
+    }
+
+    void speak() {
+        System.out.println("Speaking");
+    }
+
+    void learnJava() {
+        System.out.println("Learning Java");
+    }
+}
