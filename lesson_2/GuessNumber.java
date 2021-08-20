36ceb995224b6594be1a3367a5268870775cfb42
Kirill Zinabutdinov <zinabutdinov.ka@gmail.com>
2021-08-20 16:01:18 +0300

    Реализовал классы GuessNumberTest, GuessNumber и Player

---
+++ lesson_2/GuessNumber.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class GuessNumber {
+    
+    private Player playerOne;
+    private Player playerTwo;
+    private int computerNumber;
+
+    public GuessNumber(Player playerOne, Player playerTwo) {
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
+    }
+
+    public Player getPlayerOne() {
+        return playerOne;
+    }
+
+    public void setPlayerOne(Player playerOne) {
+        this.playerOne = playerOne;
+    }
+
+    public Player getPlayerTwo() {
+        return playerTwo;
+    }
+
+    public void setPlayerTwo(Player playerTwo) {
+        this.playerTwo = playerTwo;
+    }
+
+    public int getComputerNumber() {
+        return computerNumber;
+    }
+
+    public void setComputerNumber(int computerNumber) {
+        this.computerNumber = computerNumber;
+    }
+
+    public void startGame() {
+        System.out.println("Start!");
+        Scanner scan = new Scanner(System.in);
+        Random random = new Random();
+        computerNumber = 1 + random.nextInt(100);
+        System.out.println("/*Test: computerNumber = " + computerNumber + "*/");
+        while(true) {
+            System.out.print(playerOne.getName() + ": ");
+            playerOne.setNumber(Integer.parseInt(scan.nextLine()));
+            if(playerOne.getNumber() == computerNumber) {
+                System.out.println(playerOne.getName() + " guessed the number!");
+                break;
+            } else if(playerOne.getNumber() > computerNumber) {
+                System.out.println("This number is greater than computer number");
+            } else {
+                System.out.println("This number is less than computer number");
+            }
+            
+            System.out.print(playerTwo.getName() + ": ");
+            playerTwo.setNumber(Integer.parseInt(scan.nextLine()));
+            if(playerTwo.getNumber() == computerNumber) {
+                System.out.println(playerTwo.getName() + " guessed the number!");
+                break;
+            } else if(playerTwo.getNumber() > computerNumber) {
+                System.out.println("This number is greater than computer number");
+            } else {
+                System.out.println("This number is less than computer number");
+            }
+        }
+    }
+}
---
+++ lesson_2/GuessNumberTest.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class GuessNumberTest {
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Player #1 name: ");
+        Player playerOne = new Player(scan.nextLine());
+        System.out.print("Player #2 name: ");
+        Player playerTwo = new Player(scan.nextLine());
+        GuessNumber game = new GuessNumber(playerOne, playerTwo);
+
+        String continueAnswer;
+        do {
+            game.startGame();
+            do {
+                System.out.print("Do you want to continue? [yes/no]: ");
+                continueAnswer = scan.nextLine();
+            } while (!(continueAnswer.equals("yes") || continueAnswer.equals("no")));
+        } while(continueAnswer.equals("yes"));
+    }
+}
