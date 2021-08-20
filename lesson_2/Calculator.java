ebd1209818228623160f0d4633f61ba7f9317c11
Kirill Zinabutdinov <zinabutdinov.ka@gmail.com>
2021-08-20 13:01:10 +0300

    Реализовал классы CalculatorTest и Calculator

---
+++ lesson_2/Calculator.java
@@ -0,0 +1,53 @@
+public class Calculator {
+    
+    private int num1;
+    private int num2;
+    private char mathOperator;
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+    public char getMathOperator() {
+        return mathOperator;
+    }
+
+    public void setMathOperator(char mathOperator) {
+        this.mathOperator = mathOperator;
+    }
+
+    public int calculate() {
+        switch(mathOperator) {
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            case '*':
+                return num1 * num2;
+            case '/':
+                return num1 / num2;
+            case '%':
+                return num1 % num2;
+            case '^':
+                int result = 1;
+                for(int i = 1; i <= num2; i++) {
+                    result *= num1;
+                }
+                return result;
+            default:
+                return 0;
+        }
+    }
+}
